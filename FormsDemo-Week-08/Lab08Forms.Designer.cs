fa0d1f53c5cb466000ed51cb02dc9a60b5f30f7a
avijit bagchi <ovi.mecha@gmail.com>
2019-06-26 20:41:34 -0400

    Add project files.

---
+++ FormsDemo-Week-08/Lab08Forms.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace FormsDemo_Week_08
+{
+    partial class Lab08Forms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.AgeLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.lab8Label = new System.Windows.Forms.Label();
+            this.infoBox = new System.Windows.Forms.GroupBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.infoBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.Location = new System.Drawing.Point(6, 48);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(242, 46);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Your Name:";
+            // 
+            // AgeLabel
+            // 
+            this.AgeLabel.AutoSize = true;
+            this.AgeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AgeLabel.Location = new System.Drawing.Point(6, 126);
+            this.AgeLabel.Name = "AgeLabel";
+            this.AgeLabel.Size = new System.Drawing.Size(206, 46);
+            this.AgeLabel.TabIndex = 1;
+            this.AgeLabel.Text = "Your Age:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTextBox.Location = new System.Drawing.Point(291, 48);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(244, 53);
+            this.nameTextBox.TabIndex = 2;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ageTextBox.Location = new System.Drawing.Point(291, 126);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(244, 53);
+            this.ageTextBox.TabIndex = 3;
+            // 
+            // lab8Label
+            // 
+            this.lab8Label.AutoSize = true;
+            this.lab8Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab8Label.Location = new System.Drawing.Point(35, 27);
+            this.lab8Label.Name = "lab8Label";
+            this.lab8Label.Size = new System.Drawing.Size(112, 46);
+            this.lab8Label.TabIndex = 4;
+            this.lab8Label.Text = "Lab8";
+            // 
+            // infoBox
+            // 
+            this.infoBox.Controls.Add(this.AgeLabel);
+            this.infoBox.Controls.Add(this.NameLabel);
+            this.infoBox.Controls.Add(this.ageTextBox);
+            this.infoBox.Controls.Add(this.nameTextBox);
+            this.infoBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoBox.Location = new System.Drawing.Point(43, 87);
+            this.infoBox.Name = "infoBox";
+            this.infoBox.Size = new System.Drawing.Size(671, 196);
+            this.infoBox.TabIndex = 5;
+            this.infoBox.TabStop = false;
+            this.infoBox.Text = "Info";
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitButton.Location = new System.Drawing.Point(349, 328);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(229, 58);
+            this.SubmitButton.TabIndex = 6;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputLabel.Location = new System.Drawing.Point(39, 318);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(0, 46);
+            this.OutputLabel.TabIndex = 7;
+            // 
+            // Lab08Forms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(618, 424);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.infoBox);
+            this.Controls.Add(this.lab8Label);
+            this.Name = "Lab08Forms";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Lab08Forms_Load);
+            this.infoBox.ResumeLayout(false);
+            this.infoBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label AgeLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Label lab8Label;
+        private System.Windows.Forms.GroupBox infoBox;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.Label OutputLabel;
+    }
+}
+
---
+++ FormsDemo-Week-08/Lab08Forms.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsDemo_Week_08
+{
+    public partial class Lab08Forms : Form
+    {
+        public string UserName { get; set; }
+        public float UserAge { get; set; }
+        public Lab08Forms()
+        {
+            InitializeComponent();
+        }
+
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            SubmitButton.Enabled = true;
+        }
+
+        private void Lab08Forms_Load(object sender, EventArgs e)
+        {
+            SubmitButton.Enabled = false;
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            UserName = nameTextBox.Text;
+            UserAge = float.Parse(ageTextBox.Text);
+            OutputLabel.Text = nameTextBox.Text + ", " + ageTextBox.Text;
+            nameTextBox.Text = string.Empty;
+            ageTextBox.Text = string.Empty;
+        }
+    }
+}
